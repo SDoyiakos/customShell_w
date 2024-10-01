227ab273f1bfd5feadcd5426978e2f1bc2985204
SDoyiakos <stevendoyiakos@gmail.com>
2024-10-01 13:22:31 -0500

    exiting and parsing tokens

--- solution/wsh.c
+++ solution/wsh.c
@@ -4,7 +4,9 @@
 #include <errno.h>
 #include "wsh.h"
 
-
+/**
+*	Test function merely for me to see if tokens are being grabbed properly
+**/
 void debugTokens(TokenArr my_tokens) {
 	printf("The tokens in the arr are:\n");
 	for(int i = 0;i < my_tokens.token_count;i++) {
@@ -23,7 +25,6 @@ TokenArr tokenizeString(char* my_str, int input_size) {
 		exit(-1);
 	}
 
-	
 	// Copy string
 	if(strcpy(my_str_cpy, my_str) == NULL) {
 		exit(-1);
@@ -36,8 +37,6 @@ TokenArr tokenizeString(char* my_str, int input_size) {
 	while(strtok(NULL, &DELIM) != NULL) {
 		my_tokens.token_count++;
 	}
-
-	
 
 	// Allocate token arr
 	my_tokens.tokens = malloc(my_tokens.token_count * sizeof(char*));
@@ -57,6 +56,12 @@ TokenArr tokenizeString(char* my_str, int input_size) {
 		for(int i = 1; i < my_tokens.token_count;i++) {
 			token_buf = strtok(NULL, &DELIM); // Check for errors later
 			my_tokens.tokens[i] = malloc(strlen(token_buf) + sizeof(char));
+
+			if(my_tokens.tokens[i] == NULL) {
+				printf("Error Parsing Input\n");
+				exit(-1);
+			}
+			
 			strcpy(my_tokens.tokens[i], token_buf);
 		}
 	}
@@ -78,23 +83,30 @@ void parseInputs(char* input_buffer, int* input_size) {
 			exit(-1);
 		}
 	}
+
+	// Sanitize string to not include \n
+	else if(*input_size > 1){
+		input_buffer[*input_size - 1] = '\0';
+		(*input_size)--;
+	}
 }
 
 
-int interactiveMode() {
+void interactiveMode() {
 	TokenArr my_tokens;
 	char* user_input = malloc(MAX_INPUT * sizeof(char));
 	int* input_size = malloc(sizeof(int));
 
 	// Run loop until exit
-	while(1) {
+	while(!feof(stdin)) {
 		printf("wsh> ");
-		parseInputs(user_input, input_size);
-		printf("MY STR: %s\n", user_input);
+		parseInputs(user_input, input_size);	
 		my_tokens = tokenizeString(user_input, *input_size); // Tokenize input
-		debugTokens(my_tokens);
-		
-	}	
+		runCommand(my_tokens);
+	}
+
+	exit(0);
+	
 }
 
 
@@ -102,5 +114,38 @@ int interactiveMode() {
 int main(int argc, char* argv[]) {
 	if(argc == 1) {
 		interactiveMode();
+	}
+}
+
+void wshExit() {
+	exit(0);
+}
+
+/**
+* Gets the index of a built in command in the COMMAND_ARR
+* Returns -1 if command isnt built in
+**/
+int checkBuiltIn(char* my_command) {
+	int commands_size = (sizeof(COMMAND_ARR)/sizeof(char*));
+	for(int i = 0; i < commands_size;i++) {
+		if(strcmp(my_command, COMMAND_ARR[i]) == 0) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+void runCommand(TokenArr my_tokens) {
+	char* my_command = my_tokens.tokens[0];
+
+	switch(checkBuiltIn(my_command)) {
+		case 0:
+			if(my_tokens.token_count > 1) {
+				printf("Error, exit should be used with no parameters\n");
+			}
+			else {
+				wshExit();
+			}
+			break;
 	}
 }
--- solution/wsh.h
+++ solution/wsh.h
@@ -7,5 +7,17 @@ typedef struct {
 	char** tokens;
 } TokenArr;
 
-void wsh_exit();
+// Commands must be added to end to preserve indices
+const char* COMMAND_ARR[] = 
+{
+	"exit"
+};
+
+
+// BUILT IN FUNCTIONS
+void wshExit();
+
+
+int checkBuiltIn(char* my_command);
+void runCommand(TokenArr my_tokens);
 TokenArr tokenizeString(char* my_str, int input_size);
